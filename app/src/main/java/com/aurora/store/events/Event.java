cacf095449954c5d9aec50914cc6d055918b7a47
evermind <evermind@tuxfamily.org>
2021-02-15 15:05:23 +0100

    add enum StatusType[s] + helper method

--- app/src/main/java/com/aurora/store/events/Event.java
+++ app/src/main/java/com/aurora/store/events/Event.java
@@ -20,11 +20,13 @@ public class Event {
     public Event(SubType subType, String stringExtra) {
         this.subType = subType;
         this.stringExtra = stringExtra;
+        this.status = StatusType.NOTSET.ordinal();
     }
 
     public Event(SubType subType, int intExtra) {
         this.subType = subType;
         this.intExtra = intExtra;
+        this.status = StatusType.NOTSET.ordinal();
     }
 
     public Event(SubType subType) {
@@ -42,5 +44,19 @@ public class Event {
         NETWORK_UNAVAILABLE,
         NETWORK_AVAILABLE,
         BULK_UPDATE_NOTIFY
+        BULK_UPDATE_STARTED,
+        BULK_UPDATE_STOPPED,
+        DOWNLOAD
+    }
+
+    // evermind: even so we have status types. The SubType may not even consider a StatusType
+    // so make sure you know what the sender is doing
+    public enum StatusType {
+        FAILURE,
+        SUCCESS,
+        CANCELED,
+        CANCEL,
+        API_FAILURE,
+        NOTSET
     }
 }
